598db798a9b81ca430ca9a50c391359f56116155
Mariana-n02 <93607009+Mariana-n02@users.noreply.github.com>
2022-04-21 10:34:23 +0100

    versão antiga

--- DA_Proj1/código/DA_Proj1/código/carrinha.cpp
+++
@@ -1,9 +0,0 @@
-//
-// Created by OMEN on 18/04/2022.
-//
-
-#include "carrinha.h"
-
-carrinha::carrinha(int VolMax, int PesoMax,int Custo):
-                    VolMax(VolMax), PesoMax(PesoMax), Custo(Custo){
-}
--- DA_Proj1/código/DA_Proj1/código/encomenda.cpp
+++
@@ -1,9 +0,0 @@
-//
-// Created by OMEN on 18/04/2022.
-//
-
-#include "encomenda.h"
-
-encomenda::encomenda(int Volume, int Peso, int Recompensa):
-                     Volume(Volume), Peso(Peso), Recompensa(Recompensa)
-{}
